f4233aae7f4d6a30ddfda04ff62b3caef522a263
analuciabolico <analuciabolico.dev@gmail.com>
2023-10-26 20:06:54 +0100

    add mui dependencies into project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
-import '@/assets/styles/globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import '@/assets/styles/globals.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
 interface Props {
   children: React.ReactNode
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
+export default function App() {
   return <h1>Hello, Fono IAF!</h1>
 }
